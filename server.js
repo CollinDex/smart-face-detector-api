900479ab4ac06f971baf35913184f6e00f9b6dc2
CollinDex <obetscollins@gmail.com>
2023-08-01 10:14:25 +0100

    Created Database and Fixed EndPoints

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express'); 
-const bcrypt = require('bcrypt-nodejs');
+//const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex'); //Add kenx library to connect your backend with postgres
+const { user } = require('pg/lib/defaults');
+const bcrypt = require('bcrypt'); //Setup bcrypt
+const saltRounds = 10;
 
+
+const db = require('knex')({ //Initializing kenx library connection to db
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'A36361414a',
+      database : 'smart-brain'
+    }
+  });
+
+/* db.select('*').from('users').then(data => {
+    console.log(data);
+});
+ */
 const app = express();
 
 app.use(express.urlencoded({extended:false})); //These two lines calls up middleware that parses your json so your backend will understand it
@@ -9,74 +29,87 @@ app.use(express.json());
 app.use(cors());
 
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'Amarachi',
-            email: 'jamarachi024@gmail.com',
-            password: 'amarachi024',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Dex',
-            email: 'dex@gmail.com',
-            password: 'dex',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
-
 app.get('/', (req, res)=> {
-    res.json(database.users);
+    res.send('sucess');
 });
 
 app.post('/signin', (req, res) => {
-    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-        res.json(database.users[0]);    
-    } else {
-        res.status(400).json('error logging in');
-    }
+    db.select('email','hash').from('login')
+      .where('email','=',req.body.email)
+      .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        //console.log(isValid);
+        if (isValid) {
+            return db.select('*').from('users')
+                    .where('email','=',req.body.email)
+                    .then(user=> {
+                    //    console.log(user);
+                        res.json(user[0])
+                    })
+                    .catch(err=> res.status(400).json('unable to get user'));
+        } else {
+            res.status(400).json('Wrong Credentials')
+        }        
+        })
+        .catch(err => res.status(400).json('Wrong Credentials'))
 });
 
 app.post('/register', (req, res) => {
     const {email, name, password } = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
+
+    //Setup Bcrypt hashing algorithm
+    
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        //Create a pgsql transaction so that both login and users table are always in sync and have the same data ie: you cant add to the login table without being registered
+        db.transaction(trx => { 
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                .returning('*')
+                .insert({ //kenx syntax for inserting things into database
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date()
+            })
+                .then(user => {
+                    res.json(user[0]);
+                })        
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+        })
+        .catch(err => res.status(400).json('unable to register'))
     });
-    res.json(database.users[database.users.length-1]);
 });
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params; 
-    const user = database.users.filter( user => user.id === id);
-    user.length === 0 ? res.status(404).json('No such User') : res.json(user[0]); 
-    /* let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        } 
-    });
-    if (!found) {
-        res.status(404).json("No such User");
-    } */
+    db.select('*').from('users')
+        .where({
+            id: id
+        })
+        .then(user =>{
+            user.length ? res.json(user[0]) : res.status(400).json('Not Found');
+        })
+        .catch(err => res.status(400).json('Not Found'))
 });
 
 app.put('/image', (req, res) => {
     const { id } = req.body; 
-    const user = database.users.filter( user => user.id === id);
-    user.length === 0 ? res.status(404).json('No such User') :
-    user[0].entries++;
-    res.json(user[0].entries);    
+    db('users').where('id','=',id)
+      .increment('entries',1)
+      .returning('entries')
+      .then(entries => {
+//        console.log(entries);
+          res.json(entries[0].entries);
+      })
+      .catch(err => res.status(400).json('unable to get entries'))
 });
 
 app.listen(3000, ()=> {
